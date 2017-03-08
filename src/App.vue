e502b496af78c3b1669aff3e13b329b1df5ddf50
黄耀 <906521597@qq.com>
2017-03-09 00:32:41 +0800

    删除顶部logo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
 	<div id="app">
-		<div class="main-header">
-			<img src="./assets/imgs/logo.png" />
-		</div>
-		<div class="main-content">
-			<router-view></router-view>
-		</div>
+		<router-view></router-view>
 	</div>
 </template>
 
@@ -14,15 +9,3 @@
 		name: 'app'
 	}
 </script>
-
-<style>
-	.main-header {
-		padding: 2px 0;
-		background: #2e4e37;
-	}
-	.main-header img {
-		display: block;
-		margin: 0 auto;
-		height: 40px;
-	}
-</style>
